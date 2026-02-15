cae97a28006145fc85919d90bdf6835629bd0a7a
eviruin <eviruin@lievrn>
2026-02-15 17:54:14 +0700

    Move Next.js project to main folder

--- app/components/RadarAvatar.js
+++ app/components/RadarAvatar.js
@@ -3,7 +3,13 @@ import Image from 'next/image';
 export default function RadarAvatar() {
   return (
     <div className="avatar-wrap radar-bit">
-      <Image src="/assets/about-bg.png" alt="Taofik Hidayat" width={240} height={240} className="avatar" />
+      <Image
+        src="/assets/about-bg.png"
+        alt="Taofik Hidayat"
+        width={240}
+        height={240}
+        className="avatar"
+      />
       <span className="radar" />
       <span className="radar delay" />
     </div>
